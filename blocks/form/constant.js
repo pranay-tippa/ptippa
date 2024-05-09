44b8220eababf90852fd1fe65decaf2cf8eaacce
Pranay Tippa <ptippa@adobe.com>
2024-05-09 17:29:18 +0530

    updating blocks

--- blocks/form/constant.js
+++ blocks/form/constant.js
@@ -8,12 +8,18 @@ export const defaultErrorMessages = {
   maxFileSize: 'File too large. Reduce size and try again.',
   maxItems: 'Specify a number of items equal to or less than $0.',
   minItems: 'Specify a number of items equal to or greater than $0.',
+  pattern: 'Specify the value in allowed format : $0.',
+  minLength: 'Please lengthen this text to $0 characters or more.',
+  maxLength: 'Please shorten this text to $0 characters or less.',
+  maximum: 'Value must be less than or equal to $0.',
+  minimum: 'Value must be greater than or equal to $0.',
+  required: 'Please fill in this field.',
 };
 
 // eslint-disable-next-line no-useless-escape
 export const emailPattern = '([A-Za-z0-9][._]?)+[A-Za-z0-9]@[A-Za-z0-9]+(\.?[A-Za-z0-9]){2}\.([A-Za-z0-9]{2,4})?';
 
-let submitBaseUrl = 'https://publish-p10652-e203356-cmstg.adobeaemcloud.com';
+let submitBaseUrl = '';
 
 export function setSubmitBaseUrl(url) {
   submitBaseUrl = url;
